a5104e37bc1c543fda862f9d53d22aeab14358a1
DavidFuentelsazRodriguez <davfuerod@alum.us.es>
2025-01-08 09:55:06 +0100

    Modificacion rq1

--- decide/census/models.py
+++ decide/census/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
-
+    #Comentario añadido 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
